493310992797b912ef41d1468d1895d2713bd266
Armaan Gupta <armaan@kreative.im>
2020-05-17 15:15:13 -0500

    Add head to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head';
 import Wrapper from '../components/Wrapper/Wrapper';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Pageflow</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
       <Wrapper>
         <p>Welcome to Pageflow</p>
       </Wrapper>
