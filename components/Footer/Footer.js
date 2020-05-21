2b737a0bba1753218bb12173a7af1bd131c0b2e2
Armaan Gupta <armaan@kreative.im>
2020-05-21 01:12:16 -0500

    started footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class KMSFooter extends Component {
+  render() {
+    return(
+      <div className="footer">
+        
+        <style jsx>{`
+        
+        `}</style>
+      </div>
+    );
+  };
+}
+
+export default KMSFooter;
