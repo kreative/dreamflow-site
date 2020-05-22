71b6bfb8b964a7692cf45490fc16c8631595e1ed
Armaan Gupta <guppythegod@tcff.co>
2020-05-22 16:33:11 -0500

    changes

---
+++ pages/get-started.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function GetStarted() {
+    return(
+        <div>
+            <div>
+
+            </div>
+            <style jsx>{`
+            
+            `}</style>
+        </div>
+    );
+}
