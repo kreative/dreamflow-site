27929efbc14d0a8832b73e291a906f5296e34f45
Armaan Gupta <guppythegod@tcff.co>
2020-06-04 20:08:09 -0500

    Some changes

--- pages/get-started.js
+++ pages/get-started.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Footer from "../components/Footer/Footer";
 import Wrapper from "../components/Wrapper/Wrapper";
+import JotformEmbed from 'react-jotform-embed';
 
 export default function GetStarted() {
   return (
@@ -16,10 +17,9 @@ export default function GetStarted() {
       </Head>
       <Wrapper bg="#E7F2FD" paddingTop="300px" paddingBottom="100px">
         <h1>Get Started with Dreamflow</h1>
+        <p>Fill out the form below and our team will get back to you the next business day!</p>
       </Wrapper>
-      <Wrapper bg="white" paddingBottom="150px" paddingTop="150px">
-          
-      </Wrapper>
+      <JotformEmbed src="https://form.jotform.com/jsform/201558355345154" />
       <Footer />
       <style jsx>{``}</style>
     </div>
