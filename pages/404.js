6fd9b4f2b529ec600b2cba9b7c5f3d02e7c442c3
Armaan Gupta <guppythegod@tcff.co>
2020-06-05 13:55:35 -0500

    Created a *much* better 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,46 @@
-import Head from 'next/link';
+import Button from "../components/Button/Button";
 
 const NotFoundPage = () => {
   return (
-    <div>404</div>
+    <div>
+      <div className="body">
+        <div className="not_found_contents">
+          <h1>Page not found</h1>
+          <p>The page you're looking for can't be found. Enjoy these Shibus instead :)</p>
+          <Button 
+            fill="white"
+            textColor="black"
+            radius="3px"
+            border="1px solid white"
+            padding="15px 50px"
+            raised={false}
+            grow={true}
+          >
+            Go to Homepage
+          </Button>
+        </div>
+      </div>
+      <style jsx>{`
+        p, h1 {
+          color: white;
+        }
+
+        .body {
+          background: url("https://portal-assets.kaycdn.com/shibu-cropped.gif") no-repeat center center fixed;
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          height: 100vh;
+        }
+
+        .not_found_contents {
+          padding-top: 5%;
+          padding-left: 5%;
+        }
+
+      `}</style>
+    </div>
   );
 };
 
