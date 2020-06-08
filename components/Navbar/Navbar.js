a032bcdf1e0d39f314d6a82f2f576be8778ef4e1
Armaan Gupta <armaan@kreative.im>
2020-06-07 23:33:20 -0500

    Started work on the navbar again

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import Wrapper from "../Wrapper/Wrapper";
+import { Container, Row, Col } from "react-grid-system";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 class DreamflowNavbar extends Component {
-
   render() {
     return (
       <div className="Navbar__Wrapper">
-       
+        <Wrapper bg="rgab(0,0,0,0)" paddingTop="15px" paddingBottom="15px">
+          <Container style={{padding:"0px"}} fluid>
+            <Row nogutter>
+              <Col>
+                <p>Hello</p>
+              </Col>
+              <Col style={{textAlign:"right"}}>
+                <FontAwesomeIcon icon={faBars} />
+              </Col>
+            </Row>
+          </Container>
+        </Wrapper>
         <style jsx>{`
           .Navbar__Wrapper {
             background: rgba(255, 255, 255, 0.6);
@@ -23,18 +36,6 @@ class DreamflowNavbar extends Component {
             font-family: sans-serif;
             align-items: center;
             padding: 20px 10%;
-          }
-
-          .left-col {
-            width: 50%;
-          }
-
-          .right-col {
-            width: 50%;
-          }
-
-          .links {
-            float: right;
           }
 
           .logo {
